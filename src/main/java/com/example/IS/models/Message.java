695370444736cf6fc0142ee9136ad54b201b1718
gigagibi <you@example.com>
2021-11-01 01:54:43 +0300

    finished tasks and entries, added Message and MessageRepository

--- src/main/java/com/example/IS/models/Message.java
+++ src/main/java/com/example/IS/models/Message.java
@@ -1,2 +1,34 @@
-package com.example.IS.models;public class Message {
+package com.example.IS.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "messages")
+@Getter
+@Setter
+@ToString
+public class Message {
+    @Id
+    @SequenceGenerator(name = "office_seq", sequenceName =
+            "office_sequence", allocationSize = 1)
+    @GeneratedValue(generator = "office_seq", strategy =
+            GenerationType.SEQUENCE)
+    @Column(name = "message_id")
+    private int messageId;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "sender_id", referencedColumnName = "user_id")
+    public User sender;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "receiver_id", referencedColumnName = "user_id")
+    public User receiver;
+
+    @Column(name = "text")
+    private String text;
 }
--- src/main/java/com/example/IS/repositories/MessageRepository.java
+++ src/main/java/com/example/IS/repositories/MessageRepository.java
@@ -1,2 +1,12 @@
-package com.example.IS.repositories;public interface MessageRepository {
+package com.example.IS.repositories;
+
+import com.example.IS.models.Message;
+import com.example.IS.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    List<Message> findAllByReceiver(User receiver);
+
 }
